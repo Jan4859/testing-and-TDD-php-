247c248b5acbe519e1824745112338d6cc6bab70
jan4859 <juandiaz.iesplayamar@gmail.com>
2023-01-31 13:49:38 +0100

    add addproduct adn productinquantity

--- tests/Test1/Shop/CartAltTest.php
+++ tests/Test1/Shop/CartAltTest.php
@@ -1,4 +1,5 @@
 <?php
+
 declare(strict_types=1);
 
 
@@ -10,14 +11,37 @@ class CartAltTest extends TestCase
 {
     public function testShouldInstantiateCartWithAPreselectedProduct(): void
     {
-        $product2 = $this->getProduct2('product-1', 10);
-        $product = $this->getProduct('product-1', 10);
-        $cart  = Cart::pickUpWithProduct($product, 1);
+        $product2 = $this->getProduct2('product-1', 10); //We need a product to add to our cart. In this cases with a mock
+        $product = $this->getProduct('product-1', 10); //Without mock, we use the real product class. 
+        $cart  = Cart::pickUpWithProduct($product, 1); //Instance a cart with products, it can't be empty. 
         $cart2 = Cart::pickUpWithProduct($product2, 1);
 
         $this->assertNotEmpty($cart->id());
         $this->assertEquals(1, $cart->totalProducts());
         $this->assertEquals(1, $cart2->totalProducts());
+    }
+
+
+    public function testShouldAddAProduct(): void
+    {
+        $product = $this->getProduct('product-1', 10); //We make a product.
+        $cart = Cart::pickUp(); //Pick up empty cart.
+
+        $cart->addProductInQuantity($product, 1); //We add 1 product to our cart.
+
+        $this->assertCount(1, $cart); //Check is the cart has 1 product.
+    }
+
+    public function testShouldAddAProductInQuantity(): void
+    {
+        //Same test as before but with multiple quantity(product's units).
+        $product = $this->getProduct('product-1', 10);
+        $cart = Cart::pickUp();
+
+        $cart->addProductInQuantity($product, 10);
+
+        $this->assertCount(1, $cart);
+        $this->assertEquals(10, $cart->totalProducts());
     }
 
     private function getProduct($id, $price): ProductInterface
@@ -30,11 +54,12 @@ class CartAltTest extends TestCase
      */
     private function getProduct2($id, $price): ProductInterface
     {
+        //If our collaborator is slow or it depends on issues outside the scope of the test. This is a fast simulation
         /** @var ProductInterface | MockObject $product */
         $product = $this->createMock(ProductInterface::class);
         $product->method('id')->willReturn($id);
         $product->method('price')->willReturn(floatval($price));
-        
+
         return $product;
     }
 }
