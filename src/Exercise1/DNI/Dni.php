598c8233a17c44988fe6b200278eea8f88e91219
jan4859 <juandiaz.iesplayamar@gmail.com>
2023-02-06 13:10:50 +0100

    refactoring dni second step

--- src/Exercise1/DNI/Dni.php
+++ src/Exercise1/DNI/Dni.php
@@ -6,14 +6,18 @@ namespace Exercise1\DNI;
 
 use DomainException;
 use LengthException;
+
 class Dni
 {
     private const VALID_LENGTH = 9;
 
     public function __construct(string $dni)
     {
-        if(strlen($dni) > self::VALID_LENGTH) throw new LengthException('Too long');
-        if(strlen($dni) < self::VALID_LENGTH) throw new LengthException('Too short');
+        if (\strlen($dni) !== self::VALID_LENGTH) {
+            throw new LengthException(
+                \strlen($dni) > 9 ? 'Too long' : 'Too short'
+            );
+        }
         throw new DomainException('Ends with number');
     }
 }
